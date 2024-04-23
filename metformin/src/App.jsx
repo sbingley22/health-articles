28be439c6bf49785e8d3181dbb65f2d3211c8dd6
Sean B <seanb4@hotmail.co.uk>
2024-04-23 21:19:57 +0100

    added metformin article

---
+++ metformin/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Metformin</h1>
+      <p>Metformin is a biguanide anti-hyperglycemic agent. It works by decreasing glucose production in the liver, increasing the insulin sensitivity of body tissues, and increasing GDF15 secretion, which reduces appetite and caloric intake</p>
+      <h3>CFS Patients Experiences</h3>
+      <p>
+        Seems to be pretty mixed. Most report it makes them feel worse due to lactic acid type effects or gut issues. Some say it helps.
+      </p>
+      <h3>Mechanism of Action</h3>
+      <p>
+        The molecular mechanism of metformin is not completely understood. Multiple potential mechanisms of action have been proposed: inhibition of the mitochondrial respiratory chain (complex I), activation of AMP-activated protein kinase (AMPK), inhibition of glucagon-induced elevation of cyclic adenosine monophosphate (cAMP) with reduced activation of protein kinase A (PKA), complex IV–mediated inhibition of the GPD2 variant of mitochondrial glycerol-3-phosphate dehydrogenase (thereby reducing glycerol-derived hepatic gluconeogenesis), and an effect on gut microbiota.
+      </p>
+      <p>
+        Metformin exerts an anorexiant effect in most people, decreasing caloric intake. Metformin decreases gluconeogenesis (glucose production) in the liver. Metformin inhibits basal secretion from the pituitary gland of growth hormone, adrenocorticotropic hormone, follicle stimulating hormone, and expression of proopiomelanocortin, which in part accounts for its insulin-sensitizing effect with multiple actions on tissues including the liver, skeletal muscle, endothelium, adipose tissue, and the ovaries. The average patient with type 2 diabetes has three times the normal rate of gluconeogenesis; metformin treatment reduces this by over one-third.
+      </p>
+      <p>
+        Activation of AMPK was required for metformin's inhibitory effect on liver glucose production. AMPK is an enzyme that plays an important role in insulin signaling, whole-body energy balance, and the metabolism of glucose and fats.AMPK activation is required for an increase in the expression of small heterodimer partner, which in turn inhibited the expression of the hepatic gluconeogenic genes phosphoenolpyruvate carboxykinase and glucose 6-phosphatase. Metformin is frequently used in research along with AICA ribonucleotide as an AMPK agonist. The mechanism by which biguanides increase the activity of AMPK remains uncertain: metformin increases the concentration of cytosolic adenosine monophosphate (AMP) (as opposed to a change in total AMP or total AMP/adenosine triphosphate) which could activate AMPK allosterically at high levels; a newer theory involves binding to PEN-2. Metformin inhibits cyclic AMP production, blocking the action of glucagon, and thereby reducing fasting glucose levels. Metformin also induces a profound shift in the faecal microbial community profile in diabetic mice, and this may contribute to its mode of action possibly through an effect on glucagon-like peptide-1 secretion.
+      </p>
+      <p>
+        In addition to suppressing hepatic glucose production, metformin increases insulin sensitivity, enhances peripheral glucose uptake (by inducing the phosphorylation of GLUT4 enhancer factor), decreases insulin-induced suppression of fatty acid oxidation, and decreases the absorption of glucose from the gastrointestinal tract. Increased peripheral use of glucose may be due to improved insulin binding to insulin receptors. The increase in insulin binding after metformin treatment has also been demonstrated in patients with type 2 diabetes.
+      </p>
+      <p>
+        AMPK probably also plays a role in increased peripheral insulin sensitivity, as metformin administration increases AMPK activity in skeletal muscle. AMPK is known to cause GLUT4 deployment to the plasma membrane, resulting in insulin-independent glucose uptake.Some metabolic actions of metformin do appear to occur by AMPK-independent mechanisms, however AMPK likely has a modest overall effect and its activity is not likely to directly decrease gluconeogenesis in the liver.
+      </p>
+      <p>
+        Metformin has indirect antiandrogenic effects in women with insulin resistance, such as those with PCOS, due to its beneficial effects on insulin sensitivity. It may reduce testosterone levels in such women by as much as 50%. A Cochrane review, though, found that metformin was only slightly effective for decreasing androgen levels in women with PCOS.
+      </p>
+      <p>
+        Metformin also has significant effects on the gut microbiome, such as its effect on increasing agmatine production by gut bacteria, but the relative importance of this mechanism compared to other mechanisms is uncertain.
+      </p>
+      <p>
+        Due to its effect on GLUT4 and AMPK, metformin has been described as an exercise mimetic.
+      </p>
+      <h3>Side Effects</h3>
+      <p>Metformin is generally well tolerated and most of the side effects are short term. (2-3 weeks)</p>
+      <ul>
+        <li>Diarrhea</li>
+        <li>Nausea</li>
+        <li>Abdominal Pain</li>
+        <li>Low blood sugar</li>
+        <li>High blood lactic acid</li>
+        <li>Kidney issues</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
